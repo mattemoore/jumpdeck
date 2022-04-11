fcff01ebeaa4c332ba04ba2b3b0e9a74b12088b7
giancarlo <giancarlopsk@gmail.com>
2022-04-11 17:19:05 +0200

    Disallow crawling of JS to improve Google Crawl budget

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,10 +1,25 @@
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
 
 // add your private routes here
-const exclude = ['/dashboard/*', '/settings/*', '/onboarding/*']
+const exclude = ['/dashboard/*', '/settings/*', '/onboarding/*', '/tags*']
 
 module.exports = {
   siteUrl,
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '*.json$',
+          '/*_buildManifest.js$',
+          '/*_middlewareManifest.js$',
+          '/*_ssgManifest.js$',
+          '/*.js$'
+        ]
+      },
+    ],
+  },
   exclude,
 };
