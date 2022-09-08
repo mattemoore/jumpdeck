30c087d84ca43362b692e8e2d215ab90be5ea30a
Giancarlo Buomprisco <giancarlopsk@gmail.com>
2022-09-08 18:17:26 +0200

    Do not use pino-pretty in production (#35)

--- src/core/logger/index.ts
+++ src/core/logger/index.ts
@@ -1,18 +1,31 @@
 import pino from 'pino';
+import configuration from '~/configuration';
 
 const logger = pino({
   browser: {},
   level: 'debug',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-    },
-  },
+  transport: getTransport(),
   base: {
     env: process.env.NODE_ENV,
     revision: process.env.VERCEL_GITHUB_COMMIT_SHA,
   },
 });
+
+function getTransport() {
+  if (configuration.production) {
+    return undefined;
+  }
+
+  return getPinoPrettyConfig();
+}
+
+function getPinoPrettyConfig() {
+  return {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+    },
+  };
+}
 
 export default logger;
