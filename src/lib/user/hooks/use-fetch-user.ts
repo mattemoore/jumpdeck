89d9ec5deacfd835ed856212e562edc45277bbfb
Giancarlo Buomprisco <giancarlopsk@gmail.com>
2022-09-19 11:09:23 +0200

    Added use-fetch-user hook to simplify fetching the current user's data (#42)

---
+++ src/lib/user/hooks/use-fetch-user.ts
@@ -0,0 +1,27 @@
+import { doc, DocumentReference } from 'firebase/firestore';
+import { useFirestore, useFirestoreDocData } from 'reactfire';
+import { UserData } from '~/core/session/types/user-data';
+import { useUserId } from '~/core/hooks/use-user-id';
+
+/**
+ * @name useFetchUser
+ * @description Fetch the current's user Firestore record.
+ *
+ * Usage:
+ * const { data: user } = useFetchUser();
+ *
+ * console.log(user);
+ *
+ * NB: It's best to wrap
+ * components that use this component with {@link ErrorBoundary} as userId
+ * can become undefined, which will throw a Firestore error
+ */
+function useFetchUser() {
+  const firestore = useFirestore();
+  const userId = useUserId() as string;
+  const ref = doc(firestore, `/users`, userId) as DocumentReference<UserData>;
+
+  return useFirestoreDocData(ref, { idField: 'id' });
+}
+
+export default useFetchUser;
