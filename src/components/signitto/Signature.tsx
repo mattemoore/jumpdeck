ce38d3a4fa9b64599cf16b311b25c566dc542b6b
Matt Moore <mattmoore@pop-os.localdomain>
2023-03-12 17:00:24 -0400

    table layout for signature

--- src/components/signitto/Signature.tsx
+++ src/components/signitto/Signature.tsx
@@ -19,65 +19,81 @@ function Signature(): JSX.Element {
     'company'
   )?.value;
 
-  /* TODO: Convert layout to use TABLE for better cut and pasting */
+  /* TODO: Convert layout to use flex to see if we cut and paste works for better cut and pasting */
   return (
-    <div className="my-10 flex flex-row space-x-4">
-      <div className="flex flex-col items-center space-y-3">
-        {/* TODO: Make component wrapper for Avatar as it is used here and also in SignatureLogoDetail */}
-        <Avatar.Root className="inline-flex h-20 w-20 items-center justify-center rounded-full bg-gray-400/80">
-          <Avatar.Image
-            className="h-full w-full rounded-full object-cover"
-            src={signatureImages[0].url}
-            alt="{"
-          />
-          <Avatar.Fallback
-            className="text-black flex h-full w-full items-center justify-center rounded-full bg-gray-400/80"
-            delayMs={600}
-          >
-            {signatureImages[0].label}
-          </Avatar.Fallback>
-        </Avatar.Root>
-        {company != null && (
-          <div className="w-25 max-w-20 text-center font-bold text-yellow-500">
-            {company}
-          </div>
-        )}
-      </div>
-      <div className="flex flex-col space-y-1">
-        {signatureDetails
-          .filter((detail) => detail.value !== '')
-          .map((detail, index) => {
-            return (
-              <div key={detail.id}>
-                {index === 3 && <Divider />}
-
-                <div key={detail.id}>
-                  <div className="flex flex-row items-center">
-                    {detail.type === SignatureDetailType.Phone && (
-                      <PhoneIcon className="mr-2 h-3" />
-                    )}
-                    {detail.type === SignatureDetailType.Email && (
-                      <EnvelopeIcon className="mr-2 h-3" />
-                    )}
-                    {detail.type === SignatureDetailType.URL && (
-                      <LinkIcon className="mr-2 h-3" />
-                    )}
-                    <div
-                      className={classNames({
-                        'font-bold': index === 0,
-                        'text-normal': index < 3,
-                        'text-xs': index >= 3,
-                      })}
-                    >
-                      {detail.value}
-                    </div>
+    <>
+      <table className="Table my-10 w-1/2">
+        <thead></thead>
+        <tr>
+          {signatureImages[0].url && (
+            <td align="center" valign="top">
+              <div className="pt-7">
+                <Avatar.Root className="inline-flex h-20 w-20 items-center justify-center rounded-full bg-gray-400/80">
+                  <Avatar.Image
+                    className="h-full w-full rounded-full object-cover"
+                    src={signatureImages[0].url}
+                    alt="{"
+                  />
+                  <Avatar.Fallback
+                    className="text-black flex h-full w-full items-center justify-center rounded-full bg-gray-400/80"
+                    delayMs={600}
+                  >
+                    {signatureImages[0].label}
+                  </Avatar.Fallback>
+                </Avatar.Root>
+                {company != null && (
+                  <div className="w-25 max-w-20 text-center font-bold text-yellow-500">
+                    {company}
                   </div>
-                </div>
+                )}
               </div>
-            );
-          })}
-      </div>
-    </div>
+            </td>
+          )}
+          <td>
+            <table className="Table">
+              {signatureDetails
+                .filter((detail) => detail.value !== '')
+                .map((detail, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>
+                        <div className="flex flex-row">
+                          <div>
+                            {detail.type === SignatureDetailType.Phone && (
+                              <div className="mt-0.5 mr-2">
+                                <PhoneIcon width="1em" height="1em" />
+                              </div>
+                            )}
+                            {detail.type === SignatureDetailType.Email && (
+                              <div className="mt-0.5 mr-2">
+                                <EnvelopeIcon width="1em" height="1em" />
+                              </div>
+                            )}
+                            {detail.type === SignatureDetailType.URL && (
+                              <div className="mt-0.5 mr-2">
+                                <LinkIcon width="1em" height="1em" />
+                              </div>
+                            )}
+                          </div>
+                          <div
+                            className={classNames({
+                              'font-bold': index === 0,
+                              'text-normal': index < 3,
+                              'text-xs': index >= 3,
+                            })}
+                          >
+                            {detail.value}
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </table>
+          </td>
+        </tr>
+      </table>
+    </>
   );
 }
 
