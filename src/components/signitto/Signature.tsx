56fb3f54745371c834b0f1d503fa9458f38d54bd
Matt Moore <mattmoore@pop-os.localdomain>
2023-03-15 19:00:29 -0400

    fixed more styling fixes when pasted into gmail signature setting

--- src/components/signitto/Signature.tsx
+++ src/components/signitto/Signature.tsx
@@ -22,87 +22,100 @@ function Signature(): JSX.Element {
   /* TODO: Convert layout to use flex to see if we cut and paste works for better cut and pasting */
   return (
     <>
-      <table style={{ width: '100%' }} id="signature">
-        <thead></thead>
-        <tbody>
-          <tr>
-            {signatureImages[0].url && (
-              <td align="center" valign="top">
-                <div>
-                  <img
-                    src={signatureImages[0].url}
-                    alt={signatureImages[0].label}
-                  />
-                </div>
-                <div>
-                  {company != null && (
-                    <div
-                      style={{
-                        textAlign: 'center',
-                        fontWeight: '700',
-                        color: 'rgb(234 179 8 / 1)',
-                      }}
-                    >
-                      {company}
-                    </div>
-                  )}
-                </div>
-              </td>
-            )}
-            <td>
-              <table>
-                {signatureDetails
-                  .filter((detail) => detail.value !== '')
-                  .map((detail, index) => {
-                    return (
-                      <tr key={index}>
-                        <td>
-                          <div>
+      <div id="signature">
+        <table style={{ width: '100%' }}>
+          <thead></thead>
+          <tbody>
+            <tr>
+              {signatureImages[0].url && (
+                <td style={{ padding: '0px' }} align="center" valign="top">
+                  <div>
+                    <img
+                      src={signatureImages[0].url}
+                      alt={signatureImages[0].label}
+                    />
+                  </div>
+                  <div>
+                    {company != null && (
+                      <div
+                        style={{
+                          textAlign: 'center',
+                          fontWeight: '700',
+                          color: 'rgb(234 179 8 / 1)',
+                        }}
+                      >
+                        {company}
+                      </div>
+                    )}
+                  </div>
+                </td>
+              )}
+              <td>
+                <table style={{ width: '100%' }}>
+                  {signatureDetails
+                    .filter((detail) => detail.value !== '')
+                    .map((detail, index) => {
+                      return (
+                        <tr key={index}>
+                          <td>
                             <div>
-                              {detail.type === SignatureDetailType.Phone && (
-                                <div
-                                  style={{ float: 'left' }}
-                                  className="mt-0.5 mr-2"
-                                >
-                                  <PhoneIcon width="1em" height="1em" />
-                                </div>
-                              )}
-                              {detail.type === SignatureDetailType.Email && (
-                                <div
-                                  style={{ float: 'left' }}
-                                  className="mt-0.5 mr-2"
-                                >
-                                  <EnvelopeIcon width="1em" height="1em" />
-                                </div>
-                              )}
-                              {detail.type === SignatureDetailType.URL && (
-                                <div
-                                  style={{ float: 'left' }}
-                                  className="mt-0.5 mr-2"
-                                >
-                                  <LinkIcon width="1em" height="1em" />
-                                </div>
-                              )}
+                              <div>
+                                {detail.type === SignatureDetailType.Phone && (
+                                  <div
+                                    style={{
+                                      float: 'left',
+                                      marginTop: '0.125rem',
+                                      marginRight: '0.5rem',
+                                      width: '1em',
+                                    }}
+                                  >
+                                    <PhoneIcon width="1em" height="1em" />
+                                  </div>
+                                )}
+                                {detail.type === SignatureDetailType.Email && (
+                                  <div
+                                    style={{
+                                      float: 'left',
+                                      marginTop: '0.125rem',
+                                      marginRight: '0.5rem',
+                                      width: '1em',
+                                    }}
+                                  >
+                                    <EnvelopeIcon width="1em" height="1em" />
+                                  </div>
+                                )}
+                                {detail.type === SignatureDetailType.URL && (
+                                  <div
+                                    style={{
+                                      float: 'left',
+                                      marginTop: '0.125rem',
+                                      marginRight: '0.5rem',
+                                      width: '1em',
+                                    }}
+                                  >
+                                    <LinkIcon width="1em" height="1em" />
+                                  </div>
+                                )}
+                              </div>
+                              <div
+                                style={{
+                                  fontWeight: index === 0 ? '700' : '500',
+                                  fontSize: index > 2 ? 'small' : 'medium',
+                                }}
+                              >
+                                {detail.value}
+                              </div>
                             </div>
-                            <div
-                              className={classNames({
-                                'font-bold': index === 0,
-                                'text-normal': index < 3,
-                                'text-xs': index >= 3,
-                              })}
-                            >
-                              {detail.value}
-                            </div>
-                          </div>
-                        </td>
-                      </tr>
-                    );
-                  })}
-              </table>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                </table>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
